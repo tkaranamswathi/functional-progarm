85d5f462fe9d9e821ecfec2666edeeb99fc5655a
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-19 14:55:46 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace UC2_LEAPYEAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to leap year");
+            int year;
+            Console.Write("Input an year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine("Leap Year!");
+            }
+            else
+            {
+                Console.WriteLine("Not a Leap Year!");
+            }
+        }
+    }
+}
+        }
+    }
+}
