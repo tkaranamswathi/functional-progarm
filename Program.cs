7f9e88f54e74a9af684c53221bc089d5c2ade9c7
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-19 17:15:44 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace UC9ALPHABETS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("welcome to alphabet");
+            Console.Write("Input an Alphabet (A-Z or a-z) : ");
+            int ch;
+            ch = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("The Alphabet is vowel");
+                    break;
+                default:
+                    Console.WriteLine("The Alphabet is not a vowel");
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
