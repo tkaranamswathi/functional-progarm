9917172e70ae1db92562a91de4088dfadb95ab33
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-19 15:44:52 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace UC6_REMAINDER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to quotient and remainder");
+            Console.WriteLine("please enter first number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter second number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int quotient = x / y;
+            int remainder = x % y;
+            Console.WriteLine("x:{0} y:{1}", x, y);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
