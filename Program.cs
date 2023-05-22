8500597ca992f1759cd872ed36797b222a3a223a
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-22 11:48:17 +0530

    [swathi]adding welcome message

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Globalization;
+using System.Runtime.Serialization.Formatters;
+
+namespace UC5PRIMEFACTORS
+{
+    internal class Program
+    {
+        public static void PrimeFactors(int n)
+        {
+            Console.WriteLine("welcome to factors");
+            int n, i;
+            Console.WriteLine("Enter the Number:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Factors:");
+            for (i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
