d083c8f058cf8f4748b3e4538cdef7887e87cf0e
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-22 16:31:09 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace UC3POWEROF2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to power of 2");
+            int x = 2;
+            Console.WriteLine("please enter the number to raise");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double z = Math.Pow(x, y);
+            Console.WriteLine($"power of number:{z}");
+        }
+    }
+}
