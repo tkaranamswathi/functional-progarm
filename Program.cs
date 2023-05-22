54ea8a521b3a6a5006eb81ca06106ab8e92c4b72
tkaranamswathi <134041544+tkaranamswathi@users.noreply.github.com>
2023-05-22 13:22:10 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Console.WriteLine("welcome to heads and tails");
+            Console.WriteLine("please enter number of times you want to filp a coin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int headcount = 0;
+            int tailcount = 0;
+            Random random = new Random();
+            for (int i = 1; i <= n; i++)
+            {
+                if (random.NextDouble() > 0.5)
+                {
+                    headcount++;
+                }
+                else
+                {
+                    tailcount++;
+                }
+            }
+            Console.WriteLine("For {0} filps", n);
+            Console.WriteLine("percentage of heads: " + ((double)headcount / n) * 100 + "%");
+            Console.WriteLine("percentage of tails: " + ((double)tailcount / n) * 100 + "%");
+        }
+    }
+}
         }
     }
 }
