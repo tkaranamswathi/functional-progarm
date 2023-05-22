91a23647dcb1a17fd781a2b838d180d7cedf884e
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-22 16:05:26 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace UC4HARMONIC
+{
+    internal class Program
+    {
+        static double nthHarmonic(int N)
+        {
+            double[] harmonic = new double[N + 1];
+            harmonic[1] = 1.0;
+            for (int i = 2; i <= N; i++)
+            {
+                harmonic[i] = harmonic[i - 1] + 1.0 / i;
+            }
+
+            return harmonic[N];
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number");
+           int N =Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine(nthHarmonic(N));
+        }
+    }
+}
